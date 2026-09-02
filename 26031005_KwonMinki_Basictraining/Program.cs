39a546f9e4fd8f6aa8b108a72848767bbec842c2
walkers7622 <kwon070726@gmail.com>
2026-09-02 11:49:27 +0900

    초기 커밋.

---
+++ 26031005_KwonMinki_Basictraining/Program.cs
@@ -0,0 +1,48 @@
+﻿ internal class Program
+    {
+        static void Main(string[] args)
+        {
+			//키워드, 식별자 연습
+			//string name = "권민기";
+			//string id = "26031005";
+			//string subject = "객체지향 C#";
+
+			//Console.WriteLine($"이력서");
+			//Console.WriteLine($"이름: {name}");
+			//Console.WriteLine($"학번: {id}");
+			//Console.WriteLine($"과목: {subject}");
+
+			//연산연습
+			//Console.WriteLine(10 + 2000);
+			//Console.WriteLine(10 + "2000");
+			//Console.WriteLine(Int32.Parse("10") + 2000);
+			//Console.WriteLine("10" + "2000");
+
+
+			// 연산 연습
+			//Console.WriteLine((12345 % 10)/1);
+			//Console.WriteLine((12345 % 100) / 10);
+			//Console.WriteLine((12345 % 1000) / 100);
+			//Console.WriteLine((12345 % 10000) / 1000);
+			//Console.WriteLine((12345 % 100000) / 10000);
+
+
+			// x % y = x - (x / y) * y
+			//Console.WriteLine(4 % 3);
+			//Console.WriteLine(4 % -3);      //  4 - ( 4/-3) * (-3)
+			//Console.WriteLine(-4 % 3);     //  4 - (-4/3) * (3)
+			//Console.WriteLine(-4 % -3);    // -4 - (-4/-3) *(-3)
+
+			//Console.WriteLine(12345.0 / 1000.0);
+
+			//Console.WriteLine(1 / 2);
+			//Console.WriteLine(1.0 / 2.0);
+
+			//Console.WriteLine(5.0 % 2.2);
+			//Console.WriteLine(0.6 / 1.0);
+
+			//문자형
+			Console.WriteLine("똚");
+		}
+	}
+
