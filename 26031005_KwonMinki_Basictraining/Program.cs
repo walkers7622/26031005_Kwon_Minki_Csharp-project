f3e636db0b729d8f151d76683b3c41317583d396
walkers7622 <kwon070726@gmail.com>
2026-09-02 13:53:05 +0900

    09.02 최종 연습

--- 26031005_KwonMinki_Basictraining/Program.cs
+++ 26031005_KwonMinki_Basictraining/Program.cs
@@ -42,11 +42,13 @@
 			//Console.WriteLine(0.6 / 1.0);
 
 			//문자형
-			Console.WriteLine("똚");
+			//Console.WriteLine("똚");
 
 			// 문자열
-			Console.WriteLine("이력서");
-			Console.WriteLine("\t\t이름: 권민기");
+			//Console.WriteLine("이력서");
+			//Console.WriteLine("\t\t이름: 권민기");
+
+
 			//인덱스는 0부터
 			//Console.WriteLine("안녕하세요"[0]);
 			//Console.WriteLine("안녕하세요"[1]);
@@ -54,10 +56,36 @@
 			//Console.WriteLine("안녕하세요"[4]);
 			//Console.WriteLine("안녕하세요"[1004]);
 
-			Console.WriteLine("한" + "글");
+			//Console.WriteLine("한" + "글");
 
-			Console.WriteLine('한' + '글');
+			//Console.WriteLine('난' + '0XAC00');
 
+			//boolean
+			//Console.WriteLine(true);
+			//Console.WriteLine(false);
+
+			//변수
+			//int idNumber = 1000000000;
+			//long gameMoney = 1000000000000000;
+			//double score = 98.5;
+			//char character = 'a';
+			//string message = "안녕하세요";
+
+			//idNumber = 15;
+
+			//gameMoney = 200;
+
+			//Console.WriteLine(idNumber % gameMoney);
+
+			//int a = 2147483640;
+			//int b = 52273;
+			//Console.WriteLine(a+b);
+			Console.WriteLine(int.MinValue);
+			Console.WriteLine(int.MaxValue);
+			Console.WriteLine(uint.MinValue);
+			Console.WriteLine(uint.MaxValue);
+			Console.WriteLine(long.MinValue);
+			Console.WriteLine(long.MaxValue);
 
 		}
 	}
