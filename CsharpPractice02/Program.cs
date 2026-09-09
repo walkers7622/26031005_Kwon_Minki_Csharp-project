9aea7d0f10e6105ddde3a12ea08d8b2330745fd0
walkers7622 <kwon070726@gmail.com>
2026-09-09 12:26:33 +0900

    3주차 최종 끝!!!!!!!

--- CsharpPractice02/Program.cs
+++ CsharpPractice02/Program.cs
@@ -10,12 +10,38 @@ namespace CsharpPractice02
 
             //Console.WriteLine("Hello, World!");
 
-            var val1 = 1000L;
-            var val2 = 2000L;
+            //암시적 형변환
 
-            int valc = (int)(val1 + val2);
+            //var val1 = 1000;
+            //var val2 = 2000;
 
-        }
+            //int valc = (int)(val1 + val2);
+
+            //long valc = val1 + val2;
+            //float valf = valc;
+            //double vald = valf;
+
+            //valf = (float)vald;
+            //valc = (long)valf;
+            //val1 = (int)valc;
+
+            //var str = "20000";
+            //val1 = int.Parse(str);
+
+            //var str2 = val1 + ""; Console.WriteLine($"str2: type {str2.GetType()}");
+
+            //var b1 = bool.Parse("TRUE"); Console.WriteLine($"b1: type {b1.GetType()}");
+            //var b2 = bool.Parse("True"); Console.WriteLine($"b2: type {b2.GetType()}");
+            //var b3 = bool.Parse("true"); Console.WriteLine($"b3: type {b3.GetType()}");
+
+            int valMin = int.MinValue; Console.WriteLine(valMin);
+            valMin = -2147483648;
+            Console.WriteLine(-valMin);
+            int valMax = int.MaxValue; Console.WriteLine(valMax);
+
+
+
+			}
     }
 }
 
